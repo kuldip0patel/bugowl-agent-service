ae097b433ecedb40e537c77be156537defcc5c7c
Tirth Kothari <tirth.n.kothari@gmail.com>
2025-07-04 15:02:26 +0530

    saving data in job model

---
+++ bugowl/job/helpers.py
@@ -0,0 +1,83 @@
+from rest_framework.exceptions import ValidationError
+
+
+def validate_job_payload(data):
+	def validate_job(job):
+		if not job:
+			raise ValidationError("Missing 'job' field.")
+		required_fields = [
+			'id',
+			'uuid',
+			'status',
+			'job_type',
+			'created_at',
+			'updated_at',
+			'test_case',
+			'test_suite',
+			'experimental',
+			'Business',
+			'Project',
+			'created_by',
+		]
+		for field in required_fields:
+			if field not in job:
+				raise ValidationError(f"Missing '{field}' in job.")
+		if not isinstance(job['created_by'], dict):
+			raise ValidationError("'created_by' in job must be a dictionary.")
+		for field in ['id', 'full_name', 'email']:
+			if field not in job['created_by']:
+				raise ValidationError(f"Missing '{field}' in job['created_by'].")
+
+	def validate_case_suite(case, suite):
+		if not (suite or case):
+			raise ValidationError("Missing 'test_suite' and 'test_case' field.")
+		if case:
+			if not isinstance(case, list):
+				raise ValidationError("'test_case' must be a list.")
+			for test_case in case:
+				required_fields = ['id', 'uuid', 'test_suite', 'name', 'priority', 'browser', 'is_draft', 'test_task']
+				for field in required_fields:
+					if field not in test_case:
+						raise ValidationError(f"Missing '{field}' in test_case.")
+				if not isinstance(test_case['test_task'], list):
+					raise ValidationError("'test_task' in test_case must be a list.")
+				for idx, task in enumerate(test_case['test_task']):
+					required_fields = ['id', 'uuid', 'title', 'created_at', 'updated_at', 'created_by', 'test_data']
+					for field in required_fields:
+						if field not in task:
+							raise ValidationError(f"Missing '{field}' in test_task at index {idx}.")
+					if not isinstance(task['created_by'], int):
+						raise ValidationError(f"'created_by' in test_task at index {idx} must be an integer (user id).")
+					if task['test_data'] is not None and not isinstance(task['test_data'], int):
+						raise ValidationError(f"'test_data' in test_task at index {idx} must be an integer or null.")
+		if suite:
+			if not isinstance(suite, dict):
+				raise ValidationError("'test_suite' must be a dictionary.")
+			required_fields = ['id', 'name', 'description', 'language', 'priority', 'type']
+			for field in required_fields:
+				if field not in suite:
+					raise ValidationError(f"Missing '{field}' in test_suite.")
+
+	def validate_environment(environment):
+		if not isinstance(environment, dict):
+			raise ValidationError("'environment' must be a dictionary.")
+		required_fields = ['id', 'name', 'description', 'url']
+		for field in required_fields:
+			if field not in environment:
+				raise ValidationError(f"Missing '{field}' in environment.")
+
+	def validate_test_data(test_data):
+		if not test_data:
+			raise ValidationError("Missing 'test_data' field.")
+		if not isinstance(test_data, dict):
+			raise ValidationError("'test_data' must be a dictionary mapping test data names to data values.")
+		for test_data_name, data_value in test_data.items():
+			if not isinstance(test_data_name, str):
+				raise ValidationError('Test data name must be a string.')
+			if not isinstance(data_value, dict):
+				raise ValidationError(f"Value for '{test_data_name}' must be a dictionary (key-value pairs).")
+
+	validate_job(data.get('job'))
+	validate_case_suite(data.get('test_case'), data.get('test_suite'))
+	validate_environment(data.get('environment'))
+	validate_test_data(data.get('test_data'))
--- bugowl/job/views.py
+++ bugowl/job/views.py
@@ -6,6 +6,9 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .helpers import validate_job_payload
+from .serializer import JobSerializer
+
 logger = logging.getLogger(settings.ENV)
 
 
@@ -14,59 +17,36 @@ class ExecuteJob(APIView):
 		data = request.data
 		user = request.user
 		try:
-			self.validate_job(data.get('job'))
-			self.validate_case_suite(data.get('test_case'), data.get('test_suite'))
-			# self.validate_test_suite(data.get('test_suite'))
-			# self.validate_test_case(data.get('test_case'))
-			self.validate_environment(data.get('environment'))
-			self.validate_test_data(data.get('test_data'))
+			validate_job_payload(data)
 		except ValidationError as e:
 			logger.error('Validation Error: %s', str(e))
 			return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-		logger.info('User: %s', user.user_email)
-		logger.info('Job Data: %s', data.get('job'))
-		logger.info('Test Suite: %s', data.get('test_suite'))
-		logger.info('Test Case: %s', data.get('test_case'))
-		logger.info('Environment: %s', data.get('environment'))
-		logger.info('Test Data: %s', data.get('test_data'))
+		job_data = data.get('job')
+		test_suite_uuid = data.get('test_suite').get('uuid') if data.get('test_suite').get('uuid') else None
+		if not test_suite_uuid:
+			test_case_uuid = data.get('test_case')[0]['uuid'] if data.get('test_case') else None
+		else:
+			test_case_uuid = None
 
-		return Response({'message': 'JOB Received Successfully, Executing the JOB'}, status=status.HTTP_200_OK)
+		job_data = {
+			'uuid': job_data['uuid'],
+			'test_case_uuid': test_case_uuid,
+			'test_suite_uuid': test_suite_uuid,
+			'environment': data.get('environment'),
+			'job_type': job_data['job_type'],
+			'status': job_data['status'],
+			'business': job_data['Business'],
+			'project': job_data['Project'],
+			'created_by': job_data['created_by'],
+			'experimental': job_data['experimental'],
+			'payload': data,
+		}
 
-	def validate_job(self, job):
-		if not job:
-			raise ValidationError("Missing 'job' field.")
-		# Add specific validation for job fields here
-		# Example: if 'job_field_styles' not in job: raise ValidationError("Missing 'job_field_styles'.")
+		serializer = JobSerializer(data=job_data)
+		if not serializer.is_valid():
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	def validate_case_suite(self, case, suite):
-		if not (suite or case):
-			raise ValidationError("Missing 'test_suite' and 'test_case' field.")
+		job_instance = serializer.save()
 
-	# def validate_test_suite(self, test_suite):
-	# 	if not test_suite:
-	# 		raise ValidationError("Missing 'test_suite' field.")
-	# 	# Add specific validation for test_suite fields here
-
-	# def validate_test_case(self, test_case):
-	# 	if not test_case:
-	# 		raise ValidationError("Missing 'test_case' field.")
-	# Add specific validation for test_case fields here
-
-	def validate_environment(self, environment):
-		if not isinstance(environment, dict):
-			raise ValidationError("'environment' must be a dictionary.")
-		for key, value in environment.items():
-			# Add specific validation for environment fields here
-			pass
-
-	def validate_test_data(self, test_data):
-		if not test_data:
-			raise ValidationError("Missing 'test_data' field.")
-		if not isinstance(test_data, dict):
-			raise ValidationError("'test_data' must be a dictionary mapping test data names to data values.")
-		for test_data_name, data_value in test_data.items():
-			if not isinstance(test_data_name, str):
-				raise ValidationError('Test data name must be a string.')
-			if not isinstance(data_value, dict):
-				raise ValidationError(f"Value for '{test_data_name}' must be a dictionary (key-value pairs).")
+		return Response({'message': 'Job created successfully, Executing the Job'}, status=status.HTTP_201_CREATED)
