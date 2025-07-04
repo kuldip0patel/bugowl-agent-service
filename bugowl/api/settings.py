00910c6caff9d1c1091ed3e5371f85ef5f89d835
Tirth Kothari <146439849+TirthKothari@users.noreply.github.com>
2025-07-04 19:31:12 +0530

    Job app, Execute Job (#6)
    * job model and shifted executejob to job app
    
    * saving data in job model
    
    * job_uuid  in job
    
    * testdata can be null
    
    * agent websocket url rename
    
    * bug fix

--- bugowl/api/settings.py
+++ bugowl/api/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = [
 	'websocket',
 	'api',
 	'bugowl',
+	'job',
 	'testcase',
 	'teststep',
 	'testask',
---
+++ bugowl/job/views.py
@@ -0,0 +1,55 @@
+import logging
+
+from django.conf import settings
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .helpers import validate_job_payload
+from .serializer import JobSerializer
+
+logger = logging.getLogger(settings.ENV)
+
+
+class ExecuteJob(APIView):
+	def post(self, request, *args, **kwargs):
+		data = request.data
+		user = request.user
+		try:
+			validate_job_payload(data)
+		except ValidationError as e:
+			logger.error('Validation Error: %s', str(e))
+			return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+		try:
+			job_data = data.get('job')
+			test_suite_uuid = data.get('test_suite').get('uuid') if data.get('test_suite') else None
+			if not test_suite_uuid:
+				test_case_uuid = data.get('test_case')[0]['uuid'] if data.get('test_case') else None
+			else:
+				test_case_uuid = None
+
+			job_data = {
+				'job_uuid': job_data['uuid'],
+				'test_case_uuid': test_case_uuid,
+				'test_suite_uuid': test_suite_uuid,
+				'environment': data.get('environment'),
+				'job_type': job_data['job_type'],
+				'status': job_data['status'],
+				'business': job_data['Business'],
+				'project': job_data['Project'],
+				'created_by': job_data['created_by'],
+				'experimental': job_data['experimental'],
+				'payload': data,
+			}
+
+			serializer = JobSerializer(data=job_data)
+			if not serializer.is_valid():
+				return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+			job_instance = serializer.save()
+
+			return Response({'message': 'Job created successfully, Executing the Job'}, status=status.HTTP_201_CREATED)
+		except Exception as e:
+			logger.error('Error creating job: %s', str(e))
+			return Response({'error': 'Failed to create job', 'details': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
