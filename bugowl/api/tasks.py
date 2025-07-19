046c19367dadd2899059f879301b80e7689d1ba8
Tirth Kothari <tirth.n.kothari@gmail.com>
2025-07-19 15:09:44 +0530

    executing queued jobs on worker ready

--- bugowl/api/tasks.py
+++ bugowl/api/tasks.py
@@ -1,4 +1,10 @@
+import logging
+
 from celery import shared_task
+from celery.signals import worker_ready
+from django.conf import settings
+
+logger = logging.getLogger(settings.ENV)
 
 
 @shared_task
@@ -8,3 +14,19 @@ def health_check_task():
 	"""
 	print('AGENT: Inside a celery health check task for celery health check')
 	return True
+
+
+@worker_ready.connect
+def at_worker_ready(sender, **kwargs):
+	logger.info('Celery worker is ready.')
+	from job.models import Job
+	from job.tasks import execute_job
+
+	from .utils import JobStatusEnum
+
+	logger.info('Checking for queued jobs...')
+	queued_jobs = Job.objects.filter(status=JobStatusEnum.QUEUED.value)
+	for job in queued_jobs:
+		logger.info(f'Queued job found: {job.id}, scheduling for execution.')  # type: ignore
+		execute_job.delay(job.id)  # type: ignore
+	logger.info(f'Queued {queued_jobs.count()} jobs to celery worker on startup.')
